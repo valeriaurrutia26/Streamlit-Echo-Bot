08805eecd5a1c7a726826a9494e0891ab127bfb0
valeriaurrutia26 <5009272471@student.csn.edu>
2026-03-09 21:08:27 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,4 +1,5 @@
 import streamlit as st
-
-with st.chat_message("user"):
-  st.write("Hello👋")
+import numpy as np
+with st.chat_message("assistant"):
+  st.write("Hello human")
+  st.bar_chart(np.random.randn(30, 3))
