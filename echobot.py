119214bd7c3eabe8bc89d6d21391624050a742ec
valeriaurrutia26 <5009272471@student.csn.edu>
2026-03-09 21:23:48 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -9,7 +9,7 @@ for message in st.session_state.messages:
     st.markdown(message["content"])
 
 # React to user input
-  if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("What is up?"):
   # Display user message in chat message container
   st.chat_message("user").markdown(prompt)
   # Add user message to chat history
